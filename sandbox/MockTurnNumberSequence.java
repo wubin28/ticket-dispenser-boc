0a7d2804e207515d15c029ccebdb690d006b0f29
deer_34966B66BD <deer@cyber-dojo.org>
2015-12-10 14:02:06 +0000

    37

--- sandbox/MockTurnNumberSequence.java
+++ sandbox/MockTurnNumberSequence.java
@@ -1,6 +1,11 @@
 public class MockTurnNumberSequence extends TurnNumberSequence {
 
     private int nextTurnNumber;
+    private int count;
+    
+    public MockTurnNumberSequence() {
+        count = 0;
+    }
 
     public void arrangeNextTurnNumber(int nextTurnNumber) {
         this.nextTurnNumber = nextTurnNumber;
@@ -8,9 +13,13 @@ public class MockTurnNumberSequence extends TurnNumberSequence {
 
     @Override
     public int getNextTurnNumber() {
+        count++;
         return this.nextTurnNumber;
     }
 
     public void verifyMethodGetNextTurnNumberCalled() {
+        if (count != 1) {
+            throw new IllegalStateException("the method MockTurnNumberSequence.getNextTurnNumber() should be called once.");
+        }
     }
 }
