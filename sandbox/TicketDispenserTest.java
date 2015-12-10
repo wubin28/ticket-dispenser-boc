bb8b2542ec74244e82b46a65547d788a2adb921a
deer_34966B66BD <deer@cyber-dojo.org>
2015-12-10 13:34:09 +0000

    19

--- sandbox/TicketDispenserTest.java
+++ sandbox/TicketDispenserTest.java
@@ -33,6 +33,19 @@ public class TicketDispenserTest {
                 turnNumberOfPreviousTicketFromAnotherDispenser);
     }
 
+    @Ignore
+    public void the_dispenser_should_print_a_ticket_with_a_given_turn_number(){
+       
+        //Given
+        TurnNumberSequence sequence = mock(TurnNumberSequence.class);
+        when(TurnNumberSequence.getNextTurnNumber()).thenReturn(1);
+        //When
+        TicketDispenser dispenser = new TicketDispenser();
+        int ticketNumber = dispenser.getTurnTicket().getTurnNumber();
+        
+    }
+
+    @Test
     public void the_dispenser_should_print_a_ticket_with_a_given_turn_number(){
        
         //Given
