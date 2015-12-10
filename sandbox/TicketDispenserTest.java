87737e0414f45c19ff079c56ecf3ee7386d5201f
deer_34966B66BD <deer@cyber-dojo.org>
2015-12-10 12:50:51 +0000

    2

--- sandbox/TicketDispenserTest.java
+++ sandbox/TicketDispenserTest.java
@@ -3,8 +3,21 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class TicketDispenserTest {
+    
     @Test
-    public void a_trivial_test() {
-        assertEquals(5, 2 + 3);
+    public void the_turn_number_of_the_new_ticket_should_be_subsequent_to_the_previous_one() {
+
+   TurnTicket  turnTicket1=new TurnTicket(); 
+ 
+   number1=turnTicket1.getTurnNumber();
+   TurnTicket  turnTicket2=new TurnTicket(); 
+   number2=turnTicket1.getTurnNumber();
+
+
+  assertEquals(number1, number2-1);
     }
+
+
+
+    //todo the_turn_number_of_the_new_ticket_should_be_subsequent_to_the_previous_one_from_another_dispensor
 }
