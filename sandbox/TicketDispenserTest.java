793be344456527b690cd7660dbd61bfa8df8ac24
deer_34966B66BD <deer@cyber-dojo.org>
2015-12-10 13:33:36 +0000

    18

--- sandbox/TicketDispenserTest.java
+++ sandbox/TicketDispenserTest.java
@@ -33,13 +33,14 @@ public class TicketDispenserTest {
                 turnNumberOfPreviousTicketFromAnotherDispenser);
     }
 
-    // TODO: the_dispenser_should_print_a_ticket_with_a_given_turn_number
-    @Test
     public void the_dispenser_should_print_a_ticket_with_a_given_turn_number(){
        
         //Given
         TurnNumberSequence sequence = mock(TurnNumberSequence.class);
-        Mockito.when(TurnNumberSequence.getNextTurnNumber()).thenReturn(1);
+        when(TurnNumberSequence.getNextTurnNumber()).thenReturn(1);
         //When
+        TicketDispenser dispenser = new TicketDispenser();
+        int ticketNumber = dispenser.getTurnTicket().getTurnNumber();
+        
     }
 }
