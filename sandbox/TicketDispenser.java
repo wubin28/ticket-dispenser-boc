0f56adbe6ef4be269c6b674ac229412b3502108d
deer_34966B66BD <deer@cyber-dojo.org>
2015-12-10 13:55:54 +0000

    32

--- sandbox/TicketDispenser.java
+++ sandbox/TicketDispenser.java
@@ -1,14 +1,18 @@
 public class TicketDispenser {
 
+    private TurnNumberSequence turnNumberSequence;
+
     public TicketDispenser() {
+        this(new TurnNumberSequence());
     }
 
     public TicketDispenser(TurnNumberSequence turnNumberSequence) {
+        this.turnNumberSequence = turnNumberSequence;
     }
 
     public TurnTicket getTurnTicket()
     {
-        int newTurnNumber = TurnNumberSequence.getNextTurnNumber();
+        int newTurnNumber = this.turnNumberSequence.getNextTurnNumber();
         TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
 
         return newTurnTicket;
