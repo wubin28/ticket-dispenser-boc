c435fffc322b34dd5b605c98dc8bbc878368e2ae
deer_34966B66BD <deer@cyber-dojo.org>
2015-12-10 12:58:11 +0000

    3

--- sandbox/TicketDispenserTest.java
+++ sandbox/TicketDispenserTest.java
@@ -6,15 +6,7 @@ public class TicketDispenserTest {
     
     @Test
     public void the_turn_number_of_the_new_ticket_should_be_subsequent_to_the_previous_one() {
-
-   TurnTicket  turnTicket1=new TurnTicket(); 
- 
-   number1=turnTicket1.getTurnNumber();
-   TurnTicket  turnTicket2=new TurnTicket(); 
-   number2=turnTicket1.getTurnNumber();
-
-
-  assertEquals(number1, number2-1);
+        assertEquals(1, turnNumberOfNewTicket - turnNumberOfPreviousTicket);
     }
 
 
